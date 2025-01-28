9de29a31b3d6523ccc2fb3992985822382626d5a
Sofie Seilnacht <SeilnachtAS@cardiff.ac.uk>
2025-01-28 19:37:42 +0000

    Split data into X and y, training and testing, and converted to tensors in preparation for HFT model

--- hate_speech_detection_hft.py
+++ hate_speech_detection_hft.py
@@ -3,26 +3,36 @@ import pandas as pd
 import os
 import transformers
 from transformers import DistilBertConfig, DistilBertTokenizer, TFDistilBertForSequenceClassification
+import matplotlib
 import matplotlib.pyplot as plt
-
-plt.ion()
+import sklearn
+from sklearn.model_selection import train_test_split
+from datasets import Dataset
 
 file_path = "/Users/sofie/.cache/kagglehub/datasets/mrmorj/hate-speech-and-offensive-language-dataset/versions/1/labeled_data.csv"
 
 # Import data from csv file using path
 data = pd.read_csv(file_path)
-print(data.columns)
 
-# Access values from a specific column, e.g., 'column_name'
-hate_speech = 'hate_speech'  # Replace with the column name you want to plot
-hate_speech_values = data[hate_speech]
+# Assign features (X) and labels (y)
+X = data["tweet"]  # Feature: the text data
+y = data["class"]  # Label: the target classes
 
-plt.plot(hate_speech_values)
-# Show the plot
-plt.title("Hate Speech Values")
-plt.xlabel("Index")
-plt.ylabel("Values")
-plt.show()
+train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Create training and testing DataFrames
+train_df = pd.DataFrame({"text": train_X, "label": train_y})
+test_df = pd.DataFrame({"text": test_X, "label": test_y})
+
+# Convert to Hugging Face Dataset
+train_ds = Dataset.from_pandas(train_df)
+test_ds = Dataset.from_pandas(test_df)
+
+# plt.plot(hate_speech_values)
+# plt.title("Hate Speech Values")
+# plt.xlabel("Index")
+# plt.ylabel("Values")
+# plt.show()
 
 
 
