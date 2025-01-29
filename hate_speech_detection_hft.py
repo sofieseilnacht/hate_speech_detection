3d5f2b6a0512ba2aebea745c3d328007af195e4f
Sofie Seilnacht <SeilnachtAS@cardiff.ac.uk>
2025-01-29 15:55:09 +0000

    Infering the data using pretrained DistilBERT to see if fine tuning of the model is needed

--- hate_speech_detection_hft.py
+++ hate_speech_detection_hft.py
@@ -1,8 +1,7 @@
-# import kagglehub
 import pandas as pd
-import os
+import tensorflow as tf
 import transformers
-from transformers import DistilBertConfig, DistilBertTokenizer, TFDistilBertForSequenceClassification
+from transformers import DistilBertTokenizer, TFDistilBertForSequenceClassification
 import matplotlib
 import matplotlib.pyplot as plt
 import sklearn
@@ -15,29 +14,29 @@ file_path = "/Users/sofie/.cache/kagglehub/datasets/mrmorj/hate-speech-and-offen
 data = pd.read_csv(file_path)
 
 # Assign features (X) and labels (y)
-X = data["tweet"]  # Feature: the text data
-y = data["class"]  # Label: the target classes
+X = data["tweet"]  # Feature: the tweets 
+y = data["class"]  # Label: the classes (0: hate speech, 1: offensive language, 2: neither)
 
 train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Create training and testing DataFrames
-train_df = pd.DataFrame({"text": train_X, "label": train_y})
-test_df = pd.DataFrame({"text": test_X, "label": test_y})
+# # Create training and testing DataFrames
+# train_df = pd.DataFrame({"text": train_X, "label": train_y})
+# test_df = pd.DataFrame({"text": test_X, "label": test_y})
 
-# Convert to Hugging Face Dataset
-train_ds = Dataset.from_pandas(train_df)
-test_ds = Dataset.from_pandas(test_df)
-
-# plt.plot(hate_speech_values)
-# plt.title("Hate Speech Values")
-# plt.xlabel("Index")
-# plt.ylabel("Values")
-# plt.show()
+# # Convert to Hugging Face Dataset
+# train_ds = Dataset.from_pandas(train_df)
+# test_ds = Dataset.from_pandas(test_df)
 
 
+model_name = "distilbert-base-uncased"
+model = TFDistilBertForSequenceClassification.from_pretrained(model_name, num_labels=3)
+tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+classes = ["hate speech", "offensive language", "neither"]
 
+# Tokenization function with padding & truncation
+def tokenize_function(examples):
+    return tokenizer(examples["text"], truncation=True, padding="max_length", max_length=128)
 
-
-# model_name = "distilbert-base-uncased"
-# model = TFDistilBertForSequenceClassification.from_pretrained(model_name, num_labels=3)
-# tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+# Apply tokenization to the datasets
+train_ds = train_ds.map(tokenize_function, batched=True)
+test_ds = test_ds.map(tokenize_function, batched=True)
